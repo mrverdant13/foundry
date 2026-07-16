6f317ae44ccedd61c59673d01249ce9013626667
Karlo Verde <karlo.verde.1996@gmail.com>
2026-07-15 19:17:07 -0500

    chore(foundry_core): release 0.0.1-dev.2

--- packages/foundry_core/lib/src/version.dart
+++ packages/foundry_core/lib/src/version.dart
@@ -1,7 +1,7 @@
 /// The Foundry core library version.
 ///
 /// Keep in sync with [pubspec.yaml](https://pub.dev/packages/foundry_core).
-const foundryCoreVersion = '0.0.1-dev.1';
+const foundryCoreVersion = '0.0.1-dev.2';
 
 /// Returns [foundryCoreVersion].
 ///
